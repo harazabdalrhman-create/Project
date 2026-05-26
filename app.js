4482a0f4057a6a28ae17defb1ad773c23421fb18
Mahmoud Hany Feyala <mahmoud34.feyala34@gmail.com>
2026-05-26 18:18:40 +0300

    Add custom 404 error page with styling and navigation

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express')
 const path = require('path')
 const userRouter = require('./routes/user')
 const adminRouter = require('./routes/admin');
+const errorController = require('./controller/error');
 const app = express()
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.static(path.join(__dirname,"public")))
 app.use(userRouter)
 app.use("/admin",adminRouter)
+app.use(errorController.get404)
 app.listen(3000)
